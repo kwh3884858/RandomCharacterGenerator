9bba0da8cb2f15ba5aaec736638d97a4917d66ff
Kong Wei Hang <wei-hang.kong@ubisoft.com>
2021-08-13 23:29:20 +0800

    first commit

---
+++ RandomCharacterGenerator/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomCharacterGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window_Loaded();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<Object> e)
+        {
+            //Perform actions when SelectedItem changes
+            MessageBox.Show(((TreeViewItem)e.NewValue).Header.ToString());
+        }
+
+        private void Window_Loaded()
+        {
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+           
+            foreach (string s in Directory.GetDirectories(path))
+            {
+                TreeViewItem dummyNode = new TreeViewItem();
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s;
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+                item.Items.Add(dummyNode);
+                item.Expanded += new RoutedEventHandler(folder_Expanded);
+                foldersItem.Items.Add(item);
+            }
+
+            foreach (string s in Directory.GetFiles(path))
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+                foldersItem.Items.Add(item);
+            }
+        }
+
+        void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            if (item.Items.Count == 1)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        TreeViewItem dummyNode = new TreeViewItem();
+                        TreeViewItem subitem = new TreeViewItem();
+                        subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                        subitem.Tag = s;
+                        subitem.FontWeight = FontWeights.Normal;
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
---
+++ RandomCharacterGenerator/TableList.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RandomCharacterGenerator
+{
+    /// <summary>
+    /// Interaction logic for TableList.xaml
+    /// </summary>
+    public partial class TableList : Window
+    {
+        public TableList()
+        {
+            InitializeComponent();
+        }
+    }
+}
